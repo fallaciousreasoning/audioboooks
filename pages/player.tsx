4260ed3b3a2de3b669f27f2f76f786801bece096
Jay Harris <jay.harris@outlook.co.nz>
2020-10-05 15:56:34 +1300

    Nicer styling

--- pages/player.tsx
+++ pages/player.tsx
@@ -1,25 +1,29 @@
 import Head from "next/head"
-import { Box, Stack, Image, Button, Icon, IconButton, Link } from "@chakra-ui/core"
+import { Box, Stack, Image, Button, Icon, IconButton, Link, Grid, Flex } from "@chakra-ui/core"
 import React from "react"
-
+import styles from '../styles/Player.module.css'
+console.log(styles)
 const Player = () => {
     return <div>
         <Head>
             <title>Now Playing</title>
         </Head>
-        <Stack shadow="md">
-            <div className="controls-row">
-                <Link href="javascript:history.back()">
+        <Flex shadow="md" flexDirection="column" className={styles.root}>
+            <Box className={styles['controls-row']} shadow="md">
+                <Link onClick={() => history.back()}>
                     <Icon name="arrow-back" />
                 </Link>
                 <IconButton isRound aria-label="Sleep Timer" icon="time" />
                 <IconButton isRound aria-label="Playback Speed" icon="repeat-clock" />
-            </div>
+            </Box>
             <Image src="album-art"></Image>
-            <div className="playback">
+            <div className={styles.cover}>
 
             </div>
-        </Stack>
+            <div className={styles.playback}>
+                Foo
+            </div>
+        </Flex>
     </div>
 };
 
