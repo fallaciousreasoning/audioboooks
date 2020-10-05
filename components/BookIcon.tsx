e171a393af43b3399eda3aa562683c45d91efcfb
Jay Harris <jay.harris@outlook.co.nz>
2020-10-05 17:37:44 +1300

    Link to now playing page

--- components/BookIcon.tsx
+++ components/BookIcon.tsx
@@ -1,5 +1,6 @@
-import { Box, Stack, Image, Progress, Flex } from "@chakra-ui/core"
-import React from "react"
+import { Flex, Image, Progress } from "@chakra-ui/core";
+import Link from 'next/link';
+import React from "react";
 
 interface Props {
     title: string;
@@ -8,11 +9,13 @@ interface Props {
 }
 
 const BookIcon = (props: Props) => {
-    return <Flex height="10em" minWidth="10em" maxWidth="10em" direction="column">
-        <div>{props.title}</div>
-        <Image src={props.cover} flexShrink={1} flexGrow={1} minHeight={0} minWidth={0}/>
-        <Progress value={props.progress * 100}/>
-    </Flex>
+    return <Link href="/player">
+        <Flex height="10em" minWidth="10em" maxWidth="10em" direction="column">
+            <div>{props.title}</div>
+            <Image src={props.cover} flexShrink={1} flexGrow={1} minHeight={0} minWidth={0} />
+            <Progress value={props.progress * 100} />
+        </Flex>
+    </Link>
 }
 
 export default BookIcon;
