2ed864c47752061896372a3b8fb705953c305dff
Jay Harris <jay.harris@outlook.co.nz>
2020-10-05 16:11:28 +1300

    More laying out

--- pages/player.tsx
+++ pages/player.tsx
@@ -1,27 +1,34 @@
 import Head from "next/head"
-import { Box, Stack, Image, Button, Icon, IconButton, Link, Grid, Flex } from "@chakra-ui/core"
+import { Box, Stack, Image, Button, Icon, IconButton, Link, Grid, Flex, Select } from "@chakra-ui/core"
 import React from "react"
-import styles from '../styles/Player.module.css'
-console.log(styles)
+
+
 const Player = () => {
     return <div>
         <Head>
             <title>Now Playing</title>
         </Head>
-        <Flex shadow="md" flexDirection="column" className={styles.root}>
-            <Box className={styles['controls-row']} shadow="md">
+        <Flex shadow="md" flexDirection="column" height="100vh">
+            <Stack alignItems="center" direction="row" shadow="md" padding={1} spacing={1}>
                 <Link onClick={() => history.back()}>
                     <Icon name="arrow-back" />
                 </Link>
                 <IconButton isRound aria-label="Sleep Timer" icon="time" />
                 <IconButton isRound aria-label="Playback Speed" icon="repeat-clock" />
+            </Stack>
+            <Box flexGrow={1}>
+                <Image src="album-art"></Image>
             </Box>
-            <Image src="album-art"></Image>
-            <div className={styles.cover}>
-
-            </div>
-            <div className={styles.playback}>
-                Foo
+            <div>
+                <Flex direction="row" padding={1}>
+                    <IconButton variant="link" aria-label="Previous Track" icon="chevron-left" />
+                    <Select variant="flushed">
+                        <option value="0">Track 1</option>
+                        <option value="1">Track 2</option>
+                        <option value="2">Track 3</option>
+                    </Select>
+                    <IconButton variant="link" aria-label="Next Track" icon="chevron-right" />
+                </Flex>
             </div>
         </Flex>
     </div>
